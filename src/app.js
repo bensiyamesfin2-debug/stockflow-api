a79f2dae1e34bcf570515552e671f4bfae09c02f
bensiyamesfin2-debug <bensiyamesfin2@gmail.com>
2026-08-15 17:17:30 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -79,7 +79,7 @@ app.use(
   })
 );
 app.use(express.json({ limit: "1mb" }));
-
+app.use(express.static("public"));
 app.use((req, res, next) => {
   const startedAt = Date.now();
   const requestId = req.get("x-request-id") || require("crypto").randomUUID();
