33c168a9ffbb8bd91e19b35da51ba40eb01058bc
bensiyamesfin2-debug <bensiyamesfin2@gmail.com>
2026-08-15 17:31:48 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const prisma = require("./config/prisma");
@@ -79,6 +80,7 @@ app.use(
   })
 );
 app.use(express.json({ limit: "1mb" }));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.static("public"));
 app.use((req, res, next) => {
   const startedAt = Date.now();
